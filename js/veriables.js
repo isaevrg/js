e581858b33a4a9a3f4c5ec273ea62196698edec8
Roman <isaevrg@gmail.com>
2021-05-25 23:02:54 +0400

    Добавил новый репозитарий для js

---
+++ js/veriables.js
@@ -0,0 +1,26 @@
+// let name = "Roman";
+
+// var a = 6;
+
+
+// while (counter <= n) {
+//     result = result * counter;
+//     counter = counter + 1;
+//     console.log(result)
+// }
+
+
+// const skip = str => {
+//     let i = 0;
+//     let result = '';
+//     while (i < str.length) {
+//         result = result + str[i];
+//         i = i + 2;
+//     }
+//     return result;
+// };
+// console.log(skip("California"));
+
+// let a;
+// console.log(a);
+// console.log(typeof a);
